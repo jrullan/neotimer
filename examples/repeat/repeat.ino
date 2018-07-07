377ebc1ff9a5253660b7ea0eae8dc3adc38d026a
jrullan <jerullan@yahoo.com>
2018-07-07 07:27:06 -0400

    Update repeat.ino
    Updated the example to reflect the changes in the functionality of repeat.

--- examples/repeat/repeat.ino
+++ examples/repeat/repeat.ino
@@ -1,15 +1,37 @@
 #include <neotimer.h>
 
-Neotimer mytimer = Neotimer(1000);
+Neotimer mytimer = Neotimer(1000); // Set timer's preset to 1s
+
+int count = 0;
+const int BTN1 = 5;
+const int LED = 13;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(D13,OUTPUT);
+  pinMode(BTN1,INPUT);
+  pinMode(LED,OUTPUT);
 }
 
 void loop(){
-  if(mytimer.repeat(2000)){
-    Serial.println("Calling this periodically each two seconds");
-    digitalWrite(D13,!digitalRead(D13)); // Let's blink each two seconds
+  
+  if(mytimer.repeat(10)){
+    
+    digitalWrite(LED,!digitalRead(LED)); // Let's blink each second
+
+    Serial.print("Calling this ");
+    Serial.print(++count);
+    Serial.print(" times: LED is ");    
+    if(digitalRead(LED)){ 
+      Serial.println("ON");
+    }else{
+      Serial.println("OFF");
+    }
+
   }
+
+  if(digitalRead(BTN1) == HIGH){   
+    mytimer.repeatReset(); // If a button is pressed reset the repeat counter
+    count=0;
+  }
+
 }
